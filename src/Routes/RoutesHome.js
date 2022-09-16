0f89fbf8a14d994b93093258f9f2d13aa1943a15
JOAO VITOR PRIMO DE OLIVEIRA <jv.primo26@gmail.com>
2022-09-16 16:10:25 -0300

    alterações realizadas no back para retornar os itens selecionados

--- src/Routes/RoutesHome.js
+++ src/Routes/RoutesHome.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getListProd, postProductBuying } from "../controllers/homecontrollers.js";
+import { getListProd, postProductBuying,getproductsSelecteds} from "../controllers/homecontrollers.js";
 import { midauth } from "../middlewares/authMid.js";
 
 const router = express.Router();
 
 router.get('/home',getListProd);
 router.use(midauth);
+router.get('/home/selecteds',getproductsSelecteds);
 router.post('/home', postProductBuying);
 
 export default router;
